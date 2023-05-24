a0afd2a445b20fcb5ee0d74b0179158dc1a42f3c
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-05-24 22:38:36 +0300

    [all][test] Make all parametrized tests in the same style.

--- src/test/java/com/codenjoy/dojo/icancode/model/gun/GunWithOverHeatParamTest.java
+++ src/test/java/com/codenjoy/dojo/icancode/model/gun/GunWithOverHeatParamTest.java
@@ -24,12 +24,14 @@ package com.codenjoy.dojo.icancode.model.gun;
 
 import com.codenjoy.dojo.icancode.TestGameSettings;
 import com.codenjoy.dojo.icancode.services.GameSettings;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -44,60 +46,60 @@ public class GunWithOverHeatParamTest {
 
     private Gun gun;
     private GameSettings settings;
-    private Params params;
-
-    public GunWithOverHeatParamTest(Params params) {
-        this.params = params;
+    
+    @Parameterized.Parameter
+    public Params params;
+    
+    @Before
+    public void before() {
         settings = new TestGameSettings();
         gun = new GunWithOverHeat(){{
             settings = GunWithOverHeatParamTest.this.settings;
         }};
     }
 
-    @Parameters
+    @Parameterized.Parameters(name = "{0}")
     public static Collection<Params> data() {
-        List<Params> data = new ArrayList<>();
-        // should shoot without recharge, pause and rest
-        data.add(new Params(
-                50,
-                "ffffffffffffffffffffffffffffffffffffffffffffffffff",
-                0,
-                0,
-                0));
+        return Arrays.asList(
+                new Params(
+                        "1. should shoot without recharge, pause and rest",
+                        50,
+                        "ffffffffffffffffffffffffffffffffffffffffffffffffff",
+                        0,
+                        0,
+                        0),
 
-        // should make delay for recharge
-        data.add(new Params(
-                50,
-                "f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-",
-                1,
-                0,
-                0));
+                new Params(
+                        "2. should make delay for recharge",
+                        50,
+                        "f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-f-",
+                        1,
+                        0,
+                        0),
 
-        // should shoot without recharge and take a rest after long queue
-        data.add(new Params(
-                50,
-                "ffffffffff-----ffffffffff-----ffffffffff-----fffff",
-                0,
-                5,
-                10));
+                new Params(
+                        "3. should shoot without recharge and take a rest after long queue",
+                        50,
+                        "ffffffffff-----ffffffffff-----ffffffffff-----fffff",
+                        0,
+                        5,
+                        10),
 
-        // should shoot with recharge and take a rest after long queue
-        data.add(new Params(
-                60,
-                "f---f---f---f---f----------f---f---f---f---f----------f---f-",
-                3,
-                10,
-                5));
+                new Params(
+                        "4. should shoot with recharge and take a rest after long queue",
+                        60,
+                        "f---f---f---f---f----------f---f---f---f---f----------f---f-",
+                        3,
+                        10,
+                        5),
 
-        // should shoot with recharge and ignore rest after long queue
-        data.add(new Params(
-                50,
-                "f-----f-----f-----f-----f-----f-----f-----f-----f-",
-                5,
-                3,
-                5));
-
-        return data;
+                new Params(
+                        "5. should shoot with recharge and ignore rest after long queue",
+                        50,
+                        "f-----f-----f-----f-----f-----f-----f-----f-----f-",
+                        5,
+                        3,
+                        5));
     }
 
     @Test
@@ -120,18 +122,25 @@ public class GunWithOverHeatParamTest {
     }
 
     static class Params {
+        String name;
         int shots;
         String expected;
         int gunRecharge;
         int gunRestTime;
         int gunShotQueue;
 
-        public Params(int shots, String expected, int gunRecharge, int gunRestTime, int gunShotQueue) {
+        public Params(String name, int shots, String expected, int gunRecharge, int gunRestTime, int gunShotQueue) {
+            this.name = name;
             this.shots = shots;
             this.expected = expected;
             this.gunRecharge = gunRecharge;
             this.gunRestTime = gunRestTime;
             this.gunShotQueue = gunShotQueue;
+        }
+
+        @Override
+        public String toString() {
+            return name;
         }
     }
 }
